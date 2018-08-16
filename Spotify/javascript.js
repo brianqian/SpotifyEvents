59f3a05615b62bd7065b2461b02e4ce8ffd30251
BQ.Mac <qian.brian@gmail.com>
2018-08-16 14:10:36 -0700

    token authentication working

--- Spotify/javascript.js
+++ Spotify/javascript.js
@@ -5,40 +5,75 @@ $(document).ready(function () {
     var client_id = '3a13316200434809bcc4a3795fc632dc'; // Your client id
     var client_secret = '16d4345bbbeb4cf6b67439e2497ca9f3'; // Your secret
 
-    var request = require('request'); // "Request" library
+    // var request = require('request'); // "Request" library
     // your application requests authorization
-    var authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
+    // var authOptions = {
+    //     url: 'https://accounts.spotify.com/api/token',
+    //     headers: {
+    //         'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    //     },
+    //     form: {
+    //         grant_type: 'client_credentials'
+    //     },
+    //     json: true
+    // };
+
+    // request.post(authOptions, function (error, response, body) {
+    //     if (!error && response.statusCode === 200) {
+
+    //         // use the access token to access the Spotify Web API
+    //         var token = body.access_token;
+    //         var options = {
+    //             url: 'https://api.spotify.com/v1/users/jmperezperez',
+    //             headers: {
+    //                 'Authorization': 'Bearer ' + token
+    //             },
+    //             json: true
+    //         };
+    //         request.get(options, function (error, response, body) {
+    //             console.log(body);
+    //         });
+    //     }
+    // });
+
+
+
+    $.ajax({
+        url: cors + baseUrl +
+            method: "POST",
         headers: {
-            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            'Authorization': 'Basic ' + btoa(client_id + ":" + client_secret),
         },
-        form: {
+        data: {
+            contentType: 'application/x-www-form-urlencoded',
             grant_type: 'client_credentials'
         },
-        json: true
-    };
+        success: function (result) {
+            //called when successful
+            console.log('success');
+        },
 
-    request.post(authOptions, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-
-            // use the access token to access the Spotify Web API
-            var token = body.access_token;
-            var options = {
-                url: 'https://api.spotify.com/v1/users/jmperezperez',
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                },
-                json: true
-            };
-            request.get(options, function (error, response, body) {
-                console.log(body);
-            });
+        error: function (result) {
+            //called when there is an error
+            console.log(result);
         }
-    });
+        // dataType: "json"
+    }).then(function (response) {
+        console.log(response);
+        var token = response.access_token;
 
-
-
-
-
-
+        $.ajax({
+            url: "https://accounts.spotify.com/api/token",
+            method: "GET",
+            headers: {
+                'Authorization': 'Bearer' + client_id + ":" + client_secret
+            },
+            form: {
+                grant_type: 'client_credentials'
+            },
+            dataType: json
+        }).then(function (response) {
+            console.log(response);
+        })
+    })
 })
